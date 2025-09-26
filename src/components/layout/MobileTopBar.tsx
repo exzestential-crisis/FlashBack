2cf65b39be732ac0bbeb3128d7e0aadbdaa6d3bb
exze-alegre <akina.alegre@gmail.com>
2025-09-26 23:15:23 +0800

    Mobile bar updates

--- src/components/layout/MobileTopBar.tsx
+++ src/components/layout/MobileTopBar.tsx
@@ -8,13 +8,13 @@ export default function MobileTopBar() {
   ];
 
   return (
-    <div className="flex sm:hidden bg-zinc-800 h-14">
+    <div className="flex sm:hidden bg-zinc-800 h-10">
       <div className="flex justify-end w-full p-4 gap-4">
-        <ImSearch className="text-zinc-400 h-7 w-7" />
+        <ImSearch className="text-zinc-400 h-6 w-6" />
 
         <KebabMenu
           options={MobileOptions}
-          size={30}
+          size={24}
           className="text-zinc-400"
         />
       </div>
